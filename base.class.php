62457ad8e71a5c9c424e230dd83319ba5a901490
daan <daan@impres.nl>
2012-02-15 15:27:52 +0100

    added documentation to our testclass

--- base.class.php
+++ base.class.php
@@ -7,11 +7,23 @@ class testClass
     public function __construct()
     {}
 
+    /**
+     * Gets the param parameter
+     *
+     * @param   string  $param
+     * @return  string 
+     */
     public function getParam($param)
     {
         return $this->_param;
     }
 
+    /**
+     * Sets the param within this class
+     *
+     * @param  string   $param
+     * @return void 
+     */
     public function setParam($param)
     {
         $this->_param = $param;
