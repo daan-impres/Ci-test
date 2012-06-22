131131c9cf02ee5befce161a34492ce200c262a9
robin <robinwageman@gmail.com>
2012-06-22 16:07:09 +0100

    query update

--- deploy/sql/scripts/check-changelog.sql
+++ deploy/sql/scripts/check-changelog.sql
@@ -1,6 +1,6 @@
 -- SELECT max(jobnumber) AS '' FROM `changelog`
 SELECT DISTINCT jobnumber
 FROM changelog
-WHERE jobnumber >=44
+WHERE jobnumber >=${JOBNUMBER}
 ORDER BY  `changelog`.`jobnumber` ASC 
 LIMIT 0 , 30
