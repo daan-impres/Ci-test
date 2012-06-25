b653564ec4210d7c15ea95c59ffd6e091109fc68
Robin Wageman <robinwageman@gmail.com>
2012-06-25 12:58:36 +0200

    retry from start

--- deploy/sql/scripts/check-changelog.sql
+++ deploy/sql/scripts/check-changelog.sql
@@ -1,6 +1,6 @@
 -- SELECT max(jobnumber) AS '' FROM `changelog`
 SELECT DISTINCT jobnumber
 FROM changelog
-WHERE jobnumber >=${JOBNUMBER}
+WHERE jobnumber > ${JOBNUMBER}
 ORDER BY  `changelog`.`jobnumber` DESC 
 LIMIT 0 , 30
